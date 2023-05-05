4bfeda0f94cdcf85f3653bc0b3c31f804fbe9075
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2023-05-06 00:50:14 +0300

    Redux: initialzed

--- pokemon-web/src/App.tsx
+++ pokemon-web/src/App.tsx
@@ -1,7 +1,13 @@
 import RealApp from "./src/App";
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "./src/redux/store";
 
 const App = () => {
-  return <RealApp />;
+  return (
+    <ReduxProvider store={store}>
+      <RealApp />
+    </ReduxProvider>
+  );
 };
 
 export default App;
