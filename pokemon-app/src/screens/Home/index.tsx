1b7f2f42054c9d9d972dfda1088a17ffec9267d7
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2023-10-02 09:12:23 +0300

    HomeScreen: changing theme takes effect for real

--- pokemon-app/src/screens/Home/index.tsx
+++ pokemon-app/src/screens/Home/index.tsx
@@ -2,28 +2,59 @@
 import { useDispatch, useSelector } from "react-redux";
 import { changeTheme } from "@redux";
 import { themeSelector } from "@redux";
-import { Box, Typography } from "@mui/material";
+import { Box, Button, IconButton, Typography } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import { Card } from "@mui/material";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
 
 export default function Home() {
   const theme = useSelector(themeSelector);
   const dispatch = useDispatch();
   const muiTheme = useTheme();
 
-  console.log(muiTheme.palette.mode);
+  //console.log(muiTheme.palette.mode);
 
   return (
     <Box>
       <Typography>Home</Typography>
-      <button
+      <Button
         onClick={() => {
           dispatch(changeTheme(theme === "light" ? "dark" : "light"));
-          //console.log(theme);
         }}
+        color={"secondary"}
       >
         Change Theme
-      </button>
+      </Button>
+      <Card></Card>
+
+      <Box
+        sx={{
+          display: "flex",
+          width: "100%",
+          alignItems: "center",
+          justifyContent: "center",
+          bgcolor: "background.default",
+          color: "text.primary",
+          borderRadius: 1,
+          p: 3,
+        }}
+      >
+        {muiTheme.palette.mode} mode
+        <IconButton
+          sx={{ ml: 1 }}
+          onClick={() => {
+            dispatch(changeTheme(theme === "light" ? "dark" : "light"));
+          }}
+          color="inherit"
+        >
+          {muiTheme.palette.mode === "dark" ? (
+            <Brightness7Icon />
+          ) : (
+            <Brightness4Icon />
+          )}
+        </IconButton>
+      </Box>
     </Box>
   );
 }
